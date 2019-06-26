52a19db0787ab25812af07d6d84b5357c0cc2f75
Anthony Ou <ou.anthony@gmail.com>
2019-06-26 00:03:09 -0600

    release v1.2.0

--- unmap.js
+++ unmap.js
@@ -1,23 +1,39 @@
 #!/usr/bin/env node
 
 const fs = require('fs-extra');
-const { resolve } = require('path');
+const {
+  resolve
+} = require('path');
 const program = require('commander');
 program
-  .option('-p, --path <p>', 'input map file')
-  .option('-f, --filter <f>', 'filter')
+  .option('-p, --path <p>', 'input source map file')
+  .option('-f, --filter <f>', 'filter out file names')
   .option('-o, --output <o>', 'output folder', './')
   .parse(process.argv);
 
-fs.readFile(program.path)
+fs.exists(program.output)
+  .then(async exists => {
+    if (!exists) {
+      await fs.mkdir(program.output)
+    }
+    return fs.readFile(program.path)
+  })
   .then(js => {
     js = JSON.parse(js.toString());
     const files = js.sources
-      .map((path, idx) => ({ path, idx }))
-      .filter(({ path }) =>
+      .map((path, idx) => ({
+        path,
+        idx
+      }))
+      .filter(({
+          path
+        }) =>
         program.filter ? path.includes(program.filter) : true
       )
-      .map(({ path, idx }) =>
+      .map(({
+          path,
+          idx
+        }) =>
         fs.outputFile(resolve(program.output, path), js.sourcesContent[idx])
       );
     return Promise.all(files);
